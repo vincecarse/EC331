b98ee39c07dba654984dee20eacec650e5ee3f5a
Vincent Carse <vince.carse@gmail.com>
2020-03-21 17:01:14 +0000

    Finally have a balanced panel

--- Data_Wranglers/VAM_panel_maker.py
+++ Data_Wranglers/VAM_panel_maker.py
@@ -9,17 +9,38 @@ panel = pd.merge(aca_file, fin_file, how = 'outer',on = ['Campus','Year'])
 panel = pd.merge(panel, info_file, how = 'outer',on = ['Campus','Year'])
 panel = panel.drop(['Unnamed: 0_x','Unnamed: 0_y','Unnamed: 0','DISTRICT_y'], axis=1)
 panel = panel.rename(columns = {'DISTRICT_x':'District'})
-elem = panel[panel['grade_span'].isin(['EE - 05','PK - 05','KG - 05'])]
 dist_type = pd.read_csv('/Users/vincentcarse/Desktop/Thesis/Texas_Education/Formatted_Data/Campus&District_Type/district0708.csv',header = 2)
 dist_type = dist_type[['District','Description']]
-elem = pd.merge(elem,dist_type, on = 'District')
+panel = pd.merge(panel,dist_type, on = 'District')
 
-elem = elem.sort_values(['Campus','Year'])
-elem = elem.replace('-1',np.nan)
-elem = elem.replace('-4',np.nan)
-elem = elem.replace('.',np.nan)
+panel = panel.sort_values(['Campus','Year'])
+panel = panel.replace('-1',np.nan)
+panel = panel.replace('-4',np.nan)
+panel = panel.replace('.',np.nan)
 
-elem.to_csv('/Users/vincentcarse/Desktop/Thesis/Texas_Education/Regression/VAM_reg/elem.csv')
+panel.to_csv('/Users/vincentcarse/Desktop/Thesis/Texas_Education/Regression/VAM_reg/panel.csv')
+
+#dropping missing value schools
+
+bal = []
+for i in panel['Campus'].unique():
+    if (panel[panel['Campus'] == i].isna().sum().sum() == 0)&(len(panel[panel['Campus'] == i]) == 9):
+        bal.append(i)
+
+balanced_panel = panel[panel['Campus'].isin(bal)]
+
+for i in balanced_panel.Campus.unique():
+    print(len(panel[panel['Campus'] == i]))
+
+
+
+
+
+
+
+
+##deprecated
+
 
 
 
