3533cfcae132509e59c9986d00ae7e7716798796
Vincent Carse <vince.carse@gmail.com>
2020-03-23 19:07:46 +0000

    names

--- Data_Wranglers/google_maps_api.py
+++ Data_Wranglers/google_maps_api.py
@@ -8,12 +8,10 @@ https://www.google.com/maps/search/?api=1&query=centurylink+field
 https://www.google.com/maps/dir/?api=1&origin=RONALD+REAGAN+MS+TX&destination=Rodriguez+Middle+School+TX&travelmode=driving
 
 
-x = pd.read_csv('/Users/vincentcarse/Desktop/Thesis/Texas_Education/Formatted_Data/Campus_Nutrition_Reimbursement/2017-18/School_Nutrition_Programs___Contact_Information_and_Site-Level_Program_Participation___Program_Year_2017-2018.csv')
-y = x['SiteName'].str.replace(' ','+')
-z = x['CEName'].str.replace(' ','+')
-a= y+'+'+z
+school_data = pd.read_csv('/Users/vincentcarse/Desktop/Thesis/Texas_Education/Formatted_Data/Campus_Nutrition_Reimbursement/2017-18/School_Nutrition_Programs___Contact_Information_and_Site-Level_Program_Participation___Program_Year_2017-2018.csv')
+school_locations = x['SiteName'].str.replace(' ','+')+'+'+x['CEName'].str.replace(' ','+')
 
-for i in a:
+for i in school_locations:
 
     driver = webdriver.Chrome(executable_path='/Users/vincentcarse/Python/chromedriver')
     driver.get('https://www.google.com/maps/search/?api=1&query='+i)
