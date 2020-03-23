f56dc8d8453dfdd1bb9624685f4aa31b5d514284
Vincent Carse <vince.carse@gmail.com>
2020-03-23 19:08:10 +0000

    cosmetic

--- Data_Wranglers/google_maps_api.py
+++ Data_Wranglers/google_maps_api.py
@@ -1,12 +1,8 @@
-#google_maps_API
+import time
+
 import pandas as pd
 import selenium
 from selenium import webdriver
-import time
-
-https://www.google.com/maps/search/?api=1&query=centurylink+field
-https://www.google.com/maps/dir/?api=1&origin=RONALD+REAGAN+MS+TX&destination=Rodriguez+Middle+School+TX&travelmode=driving
-
 
 school_data = pd.read_csv('/Users/vincentcarse/Desktop/Thesis/Texas_Education/Formatted_Data/Campus_Nutrition_Reimbursement/2017-18/School_Nutrition_Programs___Contact_Information_and_Site-Level_Program_Participation___Program_Year_2017-2018.csv')
 school_locations = x['SiteName'].str.replace(' ','+')+'+'+x['CEName'].str.replace(' ','+')
@@ -17,3 +13,10 @@ for i in school_locations:
     driver.get('https://www.google.com/maps/search/?api=1&query='+i)
     time.sleep(1)
     driver.close()
+
+
+##
+
+
+https://www.google.com/maps/search/?api=1&query=centurylink+field
+https://www.google.com/maps/dir/?api=1&origin=RONALD+REAGAN+MS+TX&destination=Rodriguez+Middle+School+TX&travelmode=driving
