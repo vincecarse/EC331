ea6f43d000612c4ff95d64fa1bd19f697af8857f
Vincent Carse <vince.carse@gmail.com>
2020-04-01 17:48:54 +0100

    .

--- eme/setup.py
+++ eme/setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="bathrooms_predictor-pkg-VINCECARSE", # Replace with your own username
+    name="EME-pkg-VINCECARSE", # Replace with your own username
     version="0.0.1",
     author="Vincent Carse",
     author_email="vincecarse@gmail.com",
-    description="Tools to extract the number of bathrooms from property descriptions.",
+    description="Tools to process data from the TEA and other sources",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/vincecarse/bathrooms_predictor",
+    url="https://github.com/vincecarse/EME",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
