f5fed1b280b4eb2953ef0f1a2119a5c0584e67f7
Vincent Carse <vince.carse@gmail.com>
2020-04-02 11:51:12 +0100

    Creating file to get basic info for adjacent districts

--- Data_Wranglers/dist_distances_cleaner.py
+++ Data_Wranglers/dist_distances_cleaner.py
@@ -69,17 +69,6 @@ small_pan['Distance_indist_min_10r'] = a2
 small_pan['Distance_alldist_miles_5r'] = b1
 small_pan['Distance_indist_miles_5r'] = b2
 
-small_pan[['adj_dist','Distance_alldist_min_10r','Distance_indist_min_10r']]
-
-
-
-small_pan[small_pan['dist_name'] == small_pan['adj_dist'][2][0].upper()]
-
-
-
-
-small_pan[[]]
-
 
 
 
---
+++ Data_Wranglers/dist_info_file.py
@@ -0,0 +1,4 @@
+a = set()
+for i in small_pan['adj_dist'].values:
+    for j in i:
+        a.add(j)
